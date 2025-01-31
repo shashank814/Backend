d0b54c995a1183a48575ad4ce6dfed7de0236587
Shashank Singh <sshingh456@gmail.com>
2025-01-31 15:01:08 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
